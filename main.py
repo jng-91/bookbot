5db836c401d00a023fb920c9237a0cf809ea173a
jng-91 <jan@gerull.eu>
2023-12-08 13:24:05 +0100

    Added "class Book" Added "count_words" Method

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
+#def main():
+#    book_path = "books/frankenstein.txt"
+#    text = get_book_text(book_path)
+#    print(text)
     
     
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+class Book:
+    def __init__(self, title, author, book_path):
+        self.title = title
+        self.author = author
+        self.book_path = book_path
+
+    def get_title(self):
+        print (self.title)
+    
+    def get_author(self):
+        print (self.author)
+    
+    def get_book_text(self):
+        with open (self.book_path) as f:
+            print(f.read())
+    
+    def count_words(self):
+        with open (self.book_path) as f:
+            tmp_book_text = f.read()
+            print (len(tmp_book_text.split()))
+
+
+
+
+def main():
+    frankenstein = Book("Frankenstein", "Marry Shelley", "books/frankenstein.txt")
+
+    frankenstein.count_words()
+
+
 main()
