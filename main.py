c14ce90773a46f193ef9ef045bc6de8e4b97fbdd
jng-91 <jan@gerull.eu>
2023-12-08 10:23:32 +0100

    Commit --> Added get_book_text function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("Grueße vom Pavianfelsen")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
